4f918e07e20f8f5a89e5280934592803f215048d
peakxie <peakxie@tencent.com>
2024-01-18 18:50:58 +0800

    feat(lc): 1.18

--- test/188.go
+++ test/188.go
@@ -1,5 +1,22 @@
-package top150
+package main
 
+import "fmt"
+
+func main() {
+	prices := []int{5, 7, 2, 7, 3, 3, 5, 3, 0}
+	k := 4
+	res := maxProfit(k, prices)
+	fmt.Println(res)
+}
+
+// 有一个用例过不了,再看看
+// prices =
+// [5,7,2,7,3,3,5,3,0]
+// k = 4
+// 输出
+// 10
+// 预期结果
+// 9
 func maxProfit(k int, prices []int) int {
 
 	n := len(prices)
@@ -13,6 +30,11 @@ func maxProfit(k int, prices []int) int {
 		buy[i] = -prices[0]
 	}
 
+	fmt.Printf("第%d天:\n", 0)
+	for ii := 0; ii < k; ii++ {
+		fmt.Printf("buy[%d](%d), sell[%d](%d)\n", ii, buy[ii], ii, sell[ii])
+	}
+
 	for i := 1; i < n; i++ {
 		buy[0] = max(buy[0], -prices[i])
 		sell[0] = max(sell[0], buy[0]+prices[i])
@@ -21,6 +43,10 @@ func maxProfit(k int, prices []int) int {
 			sell[j] = max(sell[j], buy[j]+prices[i])
 		}
 
+		fmt.Printf("第%d天:\n", i)
+		for ii := 0; ii < k; ii++ {
+			fmt.Printf("buy[%d](%d), sell[%d](%d)\n", ii, buy[ii], ii, sell[ii])
+		}
 	}
 
 	return sell[k-1]
