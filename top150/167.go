b7f7ab627420b4eaae84168fd1e03ae00f193a18
peakxie <peakxie@tencent.com>
2024-02-08 11:59:01 +0800

    feat(lc): 2.8

---
+++ top150/167.go
@@ -0,0 +1,22 @@
+package top150
+
+func twoSum(numbers []int, target int) []int {
+
+	l, r := 0, len(numbers)-1
+
+	for l < r {
+		sum := numbers[l] + numbers[r]
+		if sum == target {
+			return []int{l + 1, r + 1}
+		}
+
+		if sum > target {
+			r--
+		} else {
+			l++
+		}
+	}
+
+	return []int{-1, -1}
+
+}
