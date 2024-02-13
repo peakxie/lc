62becd1dea628a0c77f5392268c504932ce554b3
peakxie <peakxie@tencent.com>
2024-02-13 17:40:59 +0800

    feat(lc): 2.13

---
+++ top150/98.go
@@ -0,0 +1,33 @@
+package top150
+
+import "math"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+
+	val := math.MinInt
+	stack := []*TreeNode{}
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if root.Val <= val {
+			return false
+		}
+		val = root.Val
+		root = root.Right
+	}
+	return true
+}
